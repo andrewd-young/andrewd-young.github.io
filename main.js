505653bd8f8b59bb8e3f65d19e86081bf19f3c7e
Andrew Young <andrewy445@gmail.com>
2021-03-29 13:36:11 -0400

    blog

---
+++ main.js
@@ -0,0 +1,15 @@
+var array;
+
+fetch("data.json")
+  .then(response => response.json())
+  .then(json => array = json);
+
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+function load(){
+    for(var i = 0; i < array.length; i++){
+        var date = new Date(array[i].date[0], array[i].date[1], array[i].date[2]);
+        document.getElementById("content").innerHTML = document.getElementById("content").innerHTML + "<span class='title'><b>" + array[i].title + "</b></span>" + "<span class='date'>" + months[date.getMonth() - 1] + " " + date.getDate() + " " + date.getFullYear() + "</span>" + "<br>";
+    }
+}
+
